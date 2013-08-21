8e57f3a7afc49a7dafbe8a9ba4eb7df8b8e05c78
Greg Reinacker <gregr@rassoc.com>
2013-08-21 12:33:49 -0600

    initial commit

---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Tradervue WP Embed Trades ===
+Contributors: greinacker
+Donate link: http://www.rassoc.com/gregr/weblog
+Tags: tradervue, trades, trader
+Requires at least: 3.0.1
+Tested up to: 3.6
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows you to embed shared trades from Tradervue via shortcode.
+
+== Description ==
+
+This plugin will embed [shared trades](http://www.tradervue.com/shared) from Tradervue into Wordpress using a shortcode.
+
+On Tradervue, shared trades have a URL like
+
+http://www.tradervue.com/shared/trades/421249
+
+To embed that trade into Wordpress, enter the following:
+
+[trade id=421249]
+
+== Installation ==
+
+* Install using WordPress built-in Add New Plugin installer
+* Activate the plugin.
+
+== Upgrade Notice ==
+
+* 
+
+== Frequently Asked Questions ==
+
+* Where can I get help? Email support@tradervue.com for help with Tradervue or this plugin.
+
+== Screenshots ==
+
+* 
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
---
+++ tradervue-wp-embed-trades.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Tradervue Embed Trades
+Description: Allows you to embed shared trades from Tradervue
+Version: 1.0.0
+Author: Tradervue
+Author URI: http://www.tradervue.com
+License: GPL2
+*/
+
+/*  Copyright 2013 Tradervue  (email : support@tradervue.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// [trade id=12345]
+function tv_trade_func($atts) {
+    extract(shortcode_atts(array('id' => ''), $atts));
+    return "<script src='http://www.tradervue.com/sharedt.js?id={$id}'></script>";
+}
+
+add_shortcode("trade", "tv_trade_func");
